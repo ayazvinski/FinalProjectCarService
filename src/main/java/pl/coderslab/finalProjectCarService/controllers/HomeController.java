0f8d5a83ddb756e5d7f4f6c3e4f7a42e5bec4007
ayazvinski <a.yazvinski@limitless.pl>
2024-02-12 21:24:30 +0100

    Controller

--- src/main/java/pl/coderslab/finalProjectCarService/controllers/HomeController.java
+++ src/main/java/pl/coderslab/finalProjectCarService/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package pl.coderslab.finalProjectCarService.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping("/")
