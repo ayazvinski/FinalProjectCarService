85fbe850750611d9857e10ba37fb380aa17e90b6
ayazvinski <a.yazvinski@limitless.pl>
2024-02-12 17:51:10 +0100

    Project Setup

---
+++ src/main/java/pl/coderslab/finalProjectCarService/FinalProjectCarServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.finalProjectCarService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectCarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectCarServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/finalProjectCarService/FinalProjectCarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.finalProjectCarService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectCarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
